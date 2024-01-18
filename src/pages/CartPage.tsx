379ec1ff21cf42e6d906f8990dfa03b4c11612d8
Doug  Modie <dmodie@gmail.com>
2024-01-18 12:22:22 -0800

    cart refetches after successful mutation

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,17 +1,25 @@
 import { fetchCart, addItem, testFcn } from '../apis/cart';
-import { useMutation, useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { useEffect } from 'react';
 import axios from 'axios';
 
 const CartPage = () => {
-  const { data, error, isLoading } = useQuery({
+  const { data, error, isLoading, refetch } = useQuery({
     queryKey: ['cart'],
     queryFn: fetchCart,
+    enabled: false,
   });
 
-  console.log('data:', data);
+  useEffect(() => {
+    refetch();
+  }, []);
+  console.log(data);
 
   const mutation = useMutation({
     mutationFn: addItem,
+    onSuccess: async () => {
+      refetch();
+    },
   });
 
   return (
