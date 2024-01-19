677b569c087da4a49c3141bd4f9ae9a98a4b4ae6
Doug  Modie <dmodie@gmail.com>
2024-01-19 14:29:19 -0800

    cart page is showing list of relevent products POC

--- src/apis/products.tsx
+++ src/apis/products.tsx
@@ -4,7 +4,28 @@ import { Product } from '../utilities/Types';
 const fetchProducts = async () =>
   (await axios.get<Product[]>('http://localhost:3001/products')).data;
 
-export { fetchProducts };
+const fetchOnly = async (args) => {
+  const productList = args.queryKey[1].productsArray;
+  //const arrr = [1, 3];
+  const allProds = await axios.get<Product[]>('http://localhost:3001/products');
+  const products = allProds.data.filter((prod) => {
+    if (productList.includes(prod.id)) {
+      return prod;
+    }
+  });
+  // const productB: Product = {
+  //   id: 777,
+  //   name: 'werwer',
+  //   cost: 34,
+  //   image: '',
+  //   description: '',
+  // };
+  // let fruits: Product[] = [productB];
+  console.log('returning fruits: ', products);
+  return products;
+};
+
+export { fetchProducts, fetchOnly };
 
 //'https://jsonplaceholder.typicode.com/posts'
 //in ts, the <> typescript generic dumb word
--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,20 +1,61 @@
 import { fetchCart, addItem } from '../apis/cart';
-import { useMutation, useQuery } from '@tanstack/react-query';
-import { useEffect } from 'react';
+import { fetchProducts, fetchOnly } from '../apis/products';
+import { useMutation, useQuery, useQueries } from '@tanstack/react-query';
+import { useEffect, useState } from 'react';
 
 const CartPage = () => {
-  const { data, error, isLoading, refetch } = useQuery({
-    queryKey: ['cart'],
-    queryFn: fetchCart,
-    enabled: false,
+  const [prods, setProds] = useState(['sadf']);
+
+  //const [data] = useQuery(fetchOnly)
+  const {
+    data: prodData,
+    error: error0,
+    isLoading: isLoading0,
+    refetch,
+  } = useQuery({
+    queryKey: ['cartProducts', { productsArray: prods }],
+    queryFn: (prods) => fetchOnly(prods),
   });
 
-  useEffect(() => {
-    refetch();
-  }, []);
-  console.log(data);
+  // const results = useQueries({
+  //   queries: [
+  //     { queryKey: ['cart', 1], queryFn: fetchCart, staleTime: Infinity },
+  //     {
+  //       queryKey: ['products', 2],
+  //       queryFn: fetchProducts,
+  //       staleTime: Infinity,
+  //     },
+  //   ],
+  // });
+  const {
+    data: cartData,
+    error,
+    isLoading,
+  } = useQuery({
+    queryKey: ['cart'],
+    queryFn: fetchCart,
+  });
 
-  return <div>cart</div>;
+  const renderItems = () => {
+    return prodData?.map((product) => {
+      return <div>{product.cost}</div>;
+    });
+  };
+
+  useEffect(() => {
+    const prods = cartData?.data.map((item) => {
+      return item.id;
+    });
+    //console.log('arr:', arr);
+    //create array and send
+    setProds(prods);
+    refetch();
+    console.log('isLoading0:', isLoading0);
+    console.log('asdf: ', prodData);
+  }, [cartData]);
+  // console.log(results);
+
+  return <div>{renderItems()}</div>;
 };
 export default CartPage;
 
